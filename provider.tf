3f54b77f593de516203a8660b52f12b3f1ebd2b7
shivapaladugula <81111233+shivapaladugula@users.noreply.github.com>
2022-10-18 13:19:38 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,9 @@
-provider "aws" {
-    region = "us-east-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.74.0"
+       region = "us-east-1"
+    }
+  }
 }
